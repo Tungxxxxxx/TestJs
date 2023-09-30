790fe64941fe2b860757184ab530fa7dd186f1b5
TUNGPT <tungphamthanhcdt@gmail.com>
2023-09-30 13:47:42 +0700

    update unitTest-jest

--- src/lastDayOfMonth.js
+++ src/lastDayOfMonth.js
@@ -2,39 +2,26 @@
  *@function getLastDay get the last day of the month
  *@param year year input
  *@param month month input
- *@returns the last day of month format as dd/MM/yyyy
+ *@returns a number as the last day of the month
  *Created by Tungpt - 27/09/2023
  */
-function getLastDay(year, month) {
+export function getLastDay(year, month) {
   if (!Number.isInteger(year)) {
-    throw "enter an integer value for year";
+    throw 'enter an integer value for year';
   } else if (year <= 0) {
-    throw "year must be greater than 0";
+    throw 'year must be greater than 0';
   }
   if (!Number.isInteger(month)) {
-    throw "enter an integer value for month";
+    throw 'enter an integer value for month';
   } else if (month <= 0 || month > 12) {
-    throw "month must be between 1 and 12";
+    throw 'month must be between 1 and 12';
   }
-  let lastDay;
-  switch (month) {
-    case 2:
-      lastDay =
-        (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 29 : 28;
-      break;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-      lastDay = 30;
-      break;
-    default:
-      lastDay = 31;
-      break;
-  }
-  month = String(month).padStart(2, "0");
-  year = String(year).padStart(4, "0");
-  return `${lastDay}/${month}/${year}`;
+  const lastDayInMonths = {
+    2: (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 29 : 28,
+    4: 30,
+    6: 30,
+    9: 30,
+    11: 30,
+  };
+  return lastDayInMonths[month] || 31;
 }
-// exports function
-module.exports = getLastDay;
--- src/lastDayOfMonth.test.js
+++ src/lastDayOfMonth.test.js
@@ -1,46 +1,49 @@
 // Import function
-const getLastDay = require("./lastDayOfMonth");
-
-describe("getLastDay", () => {
-  it("trả về ngày cuối cùng tháng 2 năm nhuận", () => {
-    expect(getLastDay(1140, 2)).toBe("29/02/1140");
+import { getLastDay } from './lastDayOfMonth';
+describe('getLastDay(year, month)', () => {
+  it('trả về ngày cuối cùng tháng 2 khi year % 4 === 0 && year % 100 !== 0', () => {
+    expect(getLastDay(1140, 2)).toBe(29);
+  });
+  it('trả về ngày cuối cùng tháng 2 khi year % 400 === 0', () => {
+    expect(getLastDay(2400, 2)).toBe(29);
   });
 
-  it("trả về ngày cuối cùng tháng 2 năm không nhuận", () => {
-    expect(getLastDay(600, 2)).toBe("28/02/0600");
+  it('trả về ngày cuối cùng tháng 2 khi year % 4 !== 0', () => {
+    expect(getLastDay(2021, 2)).toBe(28);
+  });
+  it('trả về ngày cuối cùng tháng 2 khi year % 4 === 0 && year % 100 === 0 && year % 400 !== 0', () => {
+    expect(getLastDay(600, 2)).toBe(28);
   });
 
-  it("trả về ngày cuối cùng của tháng có 30 ngày", () => {
-    expect(getLastDay(2023, 4)).toBe("30/04/2023");
+  it('trả về ngày cuối cùng của tháng có 30 ngày', () => {
+    [4, 6, 9, 11].forEach((month) => {
+      expect(getLastDay(2023, month)).toBe(30);
+    });
   });
 
-  it("trả về ngày cuối cùng của tháng có 31 ngày", () => {
-    expect(getLastDay(90, 10)).toBe("31/10/0090");
+  it('trả về ngày cuối cùng của tháng có 31 ngày', () => {
+    [1, 3, 5, 7, 8, 10, 12].forEach((month) => {
+      expect(getLastDay(2023, month)).toBe(31);
+    });
   });
 
-  it("trả về thông báo lỗi cho năm truyền vào không phải số nguyên", () => {
-    expect(() => getLastDay("2000", 2)).toThrow(
-      "enter an integer value for year"
-    );
+  it('trả về thông báo lỗi cho năm truyền vào không phải số nguyên', () => {
+    expect(() => getLastDay('2000', 2)).toThrow('enter an integer value for year');
   });
 
-  it("trả về thông báo lỗi cho năm truyền vào <= 0", () => {
-    expect(() => getLastDay(0, 2)).toThrow("year must be greater than 0");
+  it('trả về thông báo lỗi cho năm truyền vào <= 0', () => {
+    expect(() => getLastDay(0, 2)).toThrow('year must be greater than 0');
   });
 
-  it("trả về thông báo lỗi cho tháng truyền vào không phải số nguyên", () => {
-    expect(() => getLastDay(2023, [])).toThrow(
-      "enter an integer value for month"
-    );
+  it('trả về thông báo lỗi cho tháng truyền vào không phải số nguyên', () => {
+    expect(() => getLastDay(2023, [])).toThrow('enter an integer value for month');
   });
 
-  it("trả về thông báo lỗi cho tháng truyền vào <= 0", () => {
-    expect(() => getLastDay(2023, 0)).toThrow("month must be between 1 and 12");
+  it('trả về thông báo lỗi cho tháng truyền vào <= 0', () => {
+    expect(() => getLastDay(2023, 0)).toThrow('month must be between 1 and 12');
   });
 
-  it("trả về thông báo lỗi cho tháng truyền vào > 12", () => {
-    expect(() => getLastDay(2023, 13)).toThrow(
-      "month must be between 1 and 12"
-    );
+  it('trả về thông báo lỗi cho tháng truyền vào > 12', () => {
+    expect(() => getLastDay(2023, 13)).toThrow('month must be between 1 and 12');
   });
 });
